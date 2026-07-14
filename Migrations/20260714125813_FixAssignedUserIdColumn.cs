b490d42d0391d4bb6ef2762f25b7ed1bac6f7aa8
Nashim Khan <nashimkhan272@gmail.com>
2026-07-14 18:32:13 +0530

    Fix missing AssignedUserId database column

---
+++ Migrations/20260714125813_FixAssignedUserIdColumn.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskFlow.Migrations
+{
+    public partial class FixAssignedUserIdColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AssignedUserId",
+                table: "Tasks",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AssignedUserId",
+                table: "Tasks");
+        }
+    }
+}
